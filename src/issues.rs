1304c260b3b14b05b825b275e566487de388f96e
Anthony Huang <anthuang@umich.edu>
2020-09-30 16:37:42 -0700

    Issues tests

--- src/issues.rs
+++ src/issues.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use std::clone::Clone;
 use std::fmt;
 
-#[derive(Deserialize, Serialize, Clone)]
+#[derive(Deserialize, Serialize, Clone, PartialEq)]
 pub struct Issues {
     repo: String,
     issues: Vec<Issue>,
@@ -62,7 +62,7 @@ impl fmt::Debug for Issues {
     }
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct Issue {
     title: String,
     #[serde(rename = "html_url")]
@@ -82,7 +82,92 @@ impl Issue {
     }
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 struct Label {
     name: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn label(name: &str) -> Label {
+        Label {
+            name: name.to_string(),
+        }
+    }
+
+    fn issue_with_tags(tag: Vec<Label>) -> Issue {
+        Issue {
+            title: "".to_string(),
+            url: "".to_string(),
+            labels: tag,
+            created_at: Utc.timestamp(0, 0),
+        }
+    }
+
+    fn issue_with_timestamp(sec: i64) -> Issue {
+        Issue {
+            title: "".to_string(),
+            url: "".to_string(),
+            labels: Vec::new(),
+            created_at: Utc.timestamp(sec, 0),
+        }
+    }
+
+    #[test]
+    fn test_issue_has_tag() {
+        assert!(issue_with_tags(vec![label("1")]).has_tag("1".to_string()));
+        assert!(issue_with_tags(vec![label("1"), label("2"), label("3"),]).has_tag("1".to_string()));
+        assert!(!issue_with_tags(vec![label("1")]).has_tag("2".to_string()));
+    }
+
+    #[test]
+    fn test_issues_empty() {
+        let issues = Issues::new("test".to_string(), Vec::new());
+        assert!(issues.is_empty());
+        assert_eq!(issues.with_tag("test".to_string()), issues);
+        assert_eq!(issues.created_after(Utc.timestamp(0, 0)), issues);
+    }
+
+    #[test]
+    fn test_issues_with_tag() {
+        let issues = Issues::new(
+            "test".to_string(),
+            vec![
+                issue_with_tags(vec![label("1")]),
+                issue_with_tags(vec![label("2")]),
+                issue_with_tags(vec![label("1"), label("3")]),
+            ],
+        );
+        assert_eq!(
+            issues.with_tag("1".to_string()),
+            Issues::new(
+                "test".to_string(),
+                vec![
+                    issue_with_tags(vec![label("1")]),
+                    issue_with_tags(vec![label("1"), label("3")]),
+                ],
+            )
+        );
+    }
+
+    #[test]
+    fn test_issues_created_after() {
+        let issues = Issues::new(
+            "test".to_string(),
+            vec![
+                issue_with_timestamp(1),
+                issue_with_timestamp(2),
+                issue_with_timestamp(3),
+            ],
+        );
+        assert_eq!(
+            issues.created_after(Utc.timestamp(2, 0)),
+            Issues::new(
+                "test".to_string(),
+                vec![issue_with_timestamp(2), issue_with_timestamp(3)],
+            )
+        )
+    }
 }
