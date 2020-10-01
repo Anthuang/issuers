1d9b65325eee39b00d7bd7ed3b835be23830a050
Anthony Huang <anthuang@umich.edu>
2020-09-30 17:44:54 -0700

    Repo config objects

--- src/config.rs
+++ src/config.rs
@@ -1,35 +1,33 @@
 use std::{env, fs};
 
+use anyhow::Result;
+use serde::Deserialize;
 use toml::Value;
 
-pub fn get_repos() -> Vec<(String, String)> {
+#[derive(Debug, Deserialize)]
+pub struct Repo {
+    pub repo: String,
+    pub tag: String,
+}
+
+pub fn get_repos() -> Result<Vec<Repo>> {
     let mut result_vec = Vec::new();
-    let home = env::var("HOME").expect("HOME env variable not set");
+    let home = env::var("HOME")?;
     let file_path = format!("{}/.issuers.toml", home);
     let file = match fs::read(&file_path) {
         Ok(file) => file,
         Err(_) => {
-            fs::File::create(file_path).expect("Could not create config file");
-            return Vec::new();
+            fs::File::create(file_path)?;
+            return Ok(Vec::new());
         }
     };
-    let config_toml = String::from_utf8_lossy(&file)
-        .parse::<Value>()
-        .expect("String parse failed");
+    let config_toml = String::from_utf8_lossy(&file).parse::<Value>()?;
     let config_table = config_toml
         .as_table()
         .expect("Config formatted incorrectly");
     for k in config_table.iter() {
-        result_vec.push((
-            k.1["repo"]
-                .as_str()
-                .expect("Repo must exist in repo config")
-                .to_string(),
-            k.1["tag"]
-                .as_str()
-                .expect("Tag must exist in repo config")
-                .to_string(),
-        ));
+        let repo = k.1.clone().try_into::<Repo>()?;
+        result_vec.push(repo);
     }
-    result_vec
+    Ok(result_vec)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,19 +11,19 @@ use issues::Issues;
 
 pub async fn get_issues() -> Result<Vec<Issues>> {
     let mut result_issues = Vec::new();
-    let repos = get_repos();
+    let repos = get_repos()?;
 
     for r in repos {
-        let request_url = format!("https://api.github.com/repos/{repo}/issues", repo = r.0);
+        let request_url = format!("https://api.github.com/repos/{repo}/issues", repo = r.repo);
         let client = reqwest::Client::new();
         let response = client
             .get(&request_url)
-            .query(&[("labels", r.1.clone())])
+            .query(&[("labels", r.tag.clone())])
             .header(USER_AGENT, "issuers")
             .send()
             .await?;
-        let issues = Issues::new(r.0, response.json().await?);
-        result_issues.push(issues.created_after(history::read_time()?).with_tag(r.1));
+        let issues = Issues::new(r.repo, response.json().await?);
+        result_issues.push(issues.created_after(history::read_time()?).with_tag(r.tag));
     }
     Ok(result_issues)
 }
